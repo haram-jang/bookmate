ea7760c186843dfaadd431841da7b4c8375227dc
haram-jang <hramj0316@gmail.com>
2024-11-20 11:35:59 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,10 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("📗 BookMate 도서DB 제공 알라딘 인터넷서점")
+st.title("📗 BookMate")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "도서DB 제공 알라딘 인터넷서점."
+    "https://www.aladin.co.kr/"
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
